97c67afc2b99feb6426233c14ff9e45ad2a7d608
Munsif-XPS15-Ubuntu <mustnap@gmail.com>
2021-08-24 00:44:31 +0800

    upload done

---
+++ app/Http/Controllers/DocumentController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Document;
+use Illuminate\Http\Request;
+
+class DocumentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('documents.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('documents.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'document' => 'required|mimes:txt'
+        ]);
+
+        $uploadedFile = $request->file('document');
+
+        $file = $uploadedFile->store('documents');
+
+        if(!$request->filename) {
+            $originalFilename = basename($uploadedFile->getClientOriginalName(), '.' . $uploadedFile->getClientOriginalExtension());
+
+        }
+
+        $document = new Document();
+
+        $document->filename = $originalFilename ?? $request->filename;
+        $document->location = $file;
+
+        $document->body = "";
+        $document->user_id = auth()->user()->id;
+
+        $document->save();
+
+        return redirect(route('documents.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Document  $document
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Document $document)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Document  $document
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Document $document)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Document  $document
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Document $document)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Document  $document
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Document $document)
+    {
+        //
+    }
+}
